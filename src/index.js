590d9d3c19e2774e15aafef086c10f127467c366
mcallegari10 <martin.callegari@wolox.com.ar>
2017-09-05 15:55:48 -0300

    add missing props for the upper component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
+import { PROP_TYPES } from 'constants';
 
 import Widget from './components/Widget';
 import store from '../src/store/store';
@@ -8,13 +9,21 @@ import store from '../src/store/store';
 const ConnectedWidget = props =>
   <Provider store={store}>
     <Widget
+      title={props.title}
+      subtitle={props.subtitle}
       handleNewUserMessage={props.handleNewUserMessage}
+      senderPlaceHolder={props.senderPlaceHolder}
+      stylesInjected={props.stylesInjected}
       profileAvatar={props.profileAvatar}
     />
   </Provider>;
 
 ConnectedWidget.propTypes = {
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
   handleNewUserMessage: PropTypes.func.isRequired,
+  senderPlaceHolder: PropTypes.string,
+  stylesInjected: PROP_TYPES.STYLES,
   profileAvatar: PropTypes.string
 };
 
