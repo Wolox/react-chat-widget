bc590895564135875861fe40c169ac7d592db102
Dorian30 <alvaradogdorian@gmail.com>
2019-01-30 20:39:17 -0300

    Refactored Widget layout. The component needs to be present in order to be able to 'slide it out' properly

--- src/components/Widget/layout.js
+++ src/components/Widget/layout.js
@@ -9,24 +9,23 @@ import './style.scss';
 const WidgetLayout = props => (
   <div
     className={
-      `rcw-widget-container ${props.fullScreenMode ? 'rcw-full-screen' : ''} ${props.showChat ? 'rcw-opened' : ''}`
+      `rcw-widget-container ${props.fullScreenMode ? 'rcw-full-screen' : ''}`
     }
   >
-    {props.showChat &&
-      <Conversation
-        title={props.title}
-        subtitle={props.subtitle}
-        sendMessage={props.onSendMessage}
-        senderPlaceHolder={props.senderPlaceHolder}
-        profileAvatar={props.profileAvatar}
-        toggleChat={props.onToggleConversation}
-        showChat={props.showChat}
-        showCloseButton={props.showCloseButton}
-        disabledInput={props.disabledInput}
-        autofocus={props.autofocus}
-        titleAvatar={props.titleAvatar}
-      />
-    }
+    <Conversation
+      title={props.title}
+      subtitle={props.subtitle}
+      sendMessage={props.onSendMessage}
+      senderPlaceHolder={props.senderPlaceHolder}
+      profileAvatar={props.profileAvatar}
+      toggleChat={props.onToggleConversation}
+      showChat={props.showChat}
+      showCloseButton={props.showCloseButton}
+      disabledInput={props.disabledInput}
+      autofocus={props.autofocus}
+      titleAvatar={props.titleAvatar}
+      className={props.showChat ? 'active' : 'hidden'}
+    />
     {props.customLauncher ?
       props.customLauncher(props.onToggleConversation) :
       !props.fullScreenMode &&
