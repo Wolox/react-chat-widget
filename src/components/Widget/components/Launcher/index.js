d176cc0cb2ee6ec9547bb1826faad7ecf080f9dd
umergraana <umer.farooq@graana.com>
2019-04-03 15:31:02 +0500

    designed
    shit code by umar

--- src/components/Widget/components/Launcher/index.js
+++ src/components/Widget/components/Launcher/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-import openLauncher from '@assets/launcher_button.svg';
+import openLauncher from '@assets/chotaAira.png';
 import close from '@assets/clear-button.svg';
 import Badge from './components/Badge';
 import './style.scss';
@@ -11,10 +11,17 @@ const Launcher = ({ toggle, chatOpened, badge }) =>
   <button type="button" className={chatOpened ? 'rcw-launcher rcw-hide-sm' : 'rcw-launcher'} onClick={toggle}>
     <Badge badge={badge} />
     {chatOpened ?
-      <img src={close} className="rcw-close-launcher" alt="" /> :
-      <img src={openLauncher} className="rcw-open-launcher" alt="" />
+      <span>
+        <img src={close} className="rcw-close-launcher" alt="" />
+      </span> :
+      <span>
+        <img src={openLauncher} className="rcw-open-launcher" alt="" />
+        <span>Chat with Us</span>
+      </span> 
+      
     }
-  </button>;
+  </button>
+  ;
 
 Launcher.propTypes = {
   toggle: PropTypes.func,
