58d5acce2ba6e6acce969d7b8abfb10399a2e19c
fedeGiussa25 <fedegiussa25@hotmail.com>
2020-03-03 18:00:22 -0300

    destructured props

--- src/components/Widget/layout.js
+++ src/components/Widget/layout.js
@@ -6,32 +6,47 @@ import Conversation from './components/Conversation';
 import Launcher from './components/Launcher';
 import './style.scss';
 
-const WidgetLayout = props => (
+const WidgetLayout = ({
+  title,
+  subtitle,
+  onSendMessage,
+  senderPlaceHolder,
+  profileAvatar,
+  onToggleConversation,
+  showChat,
+  showCloseButton,
+  disabledInput,
+  autofocus,
+  titleAvatar,
+  badge,
+  fullScreenMode,
+  customLauncher
+}) => (
   <div
     className={
-      `rcw-widget-container ${props.fullScreenMode ? 'rcw-full-screen' : ''}`
+      `rcw-widget-container ${fullScreenMode ? 'rcw-full-screen' : ''}`
     }
   >
     <Conversation
-      title={props.title}
-      subtitle={props.subtitle}
-      sendMessage={props.onSendMessage}
-      senderPlaceHolder={props.senderPlaceHolder}
-      profileAvatar={props.profileAvatar}
-      toggleChat={props.onToggleConversation}
-      showChat={props.showChat}
-      showCloseButton={props.showCloseButton}
-      disabledInput={props.disabledInput}
-      autofocus={props.autofocus}
-      titleAvatar={props.titleAvatar}
-      className={props.showChat ? 'active' : 'hidden'}
+      title={title}
+      subtitle={subtitle}
+      sendMessage={onSendMessage}
+      senderPlaceHolder={senderPlaceHolder}
+      profileAvatar={profileAvatar}
+      toggleChat={onToggleConversation}
+      showChat={showChat}
+      showCloseButton={showCloseButton}
+      disabledInput={disabledInput}
+      autofocus={autofocus}
+      titleAvatar={titleAvatar}
+      className={showChat ? 'active' : 'hidden'}
     />
-    {props.customLauncher ?
-      props.customLauncher(props.onToggleConversation) :
-      !props.fullScreenMode &&
+    {customLauncher ?
+      customLauncher(onToggleConversation) :
+      !fullScreenMode &&
       <Launcher
-        toggle={props.onToggleConversation}
-        badge={props.badge}
+        toggle={onToggleConversation}
+        badge={badge}
       />
     }
   </div>
